dccc8671f239706102e67e280b49864ace442a14
sharpordie <72373746+sharpordie@users.noreply.github.com>
2023-01-18 19:19:46 +0100

    small update

--- src/Minimal.ps1
+++ src/Minimal.ps1
@@ -48,7 +48,7 @@ Function Expand-Archive {
         [String] $Secrets
     )
 
-    $Starter = "7z.exe"
+    $Starter = "$Env:LocalAppData\Microsoft\WindowsApps\7z.exe"
     If (-Not (Test-Path "$Starter")) { Update-Nanazip }
     If (-Not $Deposit) { $Deposit = [IO.Directory]::CreateDirectory("$Env:Temp\$([Guid]::NewGuid().Guid)").FullName }
     If (-Not (Test-Path "$Deposit")) { New-Item "$Deposit" -ItemType Directory -EA SI }
@@ -222,6 +222,128 @@ Function Update-SysPath {
 
 #EndRegion
 
+Function Update-AndroidCmdline {
+
+    $SdkHome = "$Env:LocalAppData\Android\Sdk"
+    $Starter = "$SdkHome\cmdline-tools\latest\bin\sdkmanager.bat"
+    $Updated = Test-Path "$Starter" -NewerThan (Get-Date).AddDays(-90)
+    If (-Not $Updated) {
+        $Address = "https://developer.android.com/studio#command-tools"
+        $Release = Invoke-Scraper "HtmlContent" "$Address" "commandlinetools-win-(\d+)"
+        $Address = "https://dl.google.com/android/repository/commandlinetools-win-${Release}_latest.zip"
+        $Fetched = Invoke-Fetcher "$Address"
+        $Deposit = Expand-Archive "$Fetched"
+        New-Item "$SdkHome" -ItemType Directory -EA SI
+        Update-Temurin ; $Manager = "$Deposit\cmdline-tools\bin\sdkmanager.bat"
+        Invoke-Expression "echo $("yes " * 10) | & `"$Manager`" --sdk_root=`"$SdkHome`" `"cmdline-tools;latest`""
+    }
+
+    Invoke-Gsudo { [Environment]::SetEnvironmentVariable("ANDROID_HOME", "$Using:SdkHome", "Machine") }
+    [Environment]::SetEnvironmentVariable("ANDROID_HOME", "$SdkHome", "Process")
+    Update-SysPath "$SdkHome\cmdline-tools\latest\bin" "Machine"
+    Update-SysPath "$SdkHome\emulator" "Machine"
+    Update-SysPath "$SdkHome\platform-tools" "Machine"
+
+}
+
+Function Update-AndroidStudio {
+
+    $Starter = "$Env:ProgramFiles\Android\Android Studio\bin\studio64.exe"
+    $Current = Expand-Version "$Starter"
+    $Present = Test-Path "$Starter"
+    $Address = "https://raw.githubusercontent.com/scoopinstaller/extras/master/bucket/android-studio.json"
+    $Version = (Invoke-Scraper "JsonContent" "$Address").version
+    $Updated = [Version] "$Current" -Ge [Version] ($Version.SubString(0, 6))
+    If (-Not $Updated) {
+        $Address = "https://redirector.gvt1.com/edgedl/android/studio/install/$Version/android-studio-$Version-windows.exe"
+        $Fetched = Invoke-Fetcher "$Address"
+        Invoke-Gsudo { Start-Process "$Using:Fetched" "/S" -Wait }
+    }
+
+    If (-Not $Present) {
+        Update-AndroidCmdline
+        Invoke-Expression "echo $("yes " * 10) | sdkmanager 'build-tools;33.0.1'"
+        Invoke-Expression "echo $("yes " * 10) | sdkmanager 'emulator'"
+        Invoke-Expression "echo $("yes " * 10) | sdkmanager 'extras;intel;Hardware_Accelerated_Execution_Manager'"
+        Invoke-Expression "echo $("yes " * 10) | sdkmanager 'platform-tools'"
+        Invoke-Expression "echo $("yes " * 10) | sdkmanager 'platforms;android-33'"
+        Invoke-Expression "echo $("yes " * 10) | sdkmanager 'platforms;android-33-ext4'"
+        Invoke-Expression "echo $("yes " * 10) | sdkmanager 'sources;android-33'"
+        Invoke-Expression "echo $("yes " * 10) | sdkmanager 'system-images;android-33;google_apis;x86_64'"
+        Invoke-Expression "echo $("yes " * 10) | sdkmanager --licenses"
+        Invoke-Expression "avdmanager create avd -n 'Pixel_3_API_33' -d 'pixel_3' -k 'system-images;android-33;google_apis;x86_64'"
+    }
+
+    If (-Not $Present) {
+        Add-Type -AssemblyName System.Windows.Forms
+        Start-Process "$Starter" ; Start-Sleep 10
+        [Windows.Forms.SendKeys]::SendWait("{TAB}") ; Start-Sleep 2 ; [Windows.Forms.SendKeys]::SendWait("{ENTER}") ; Start-Sleep 20
+        [Windows.Forms.SendKeys]::SendWait("{TAB}" * 2) ; Start-Sleep 2 ; [Windows.Forms.SendKeys]::SendWait("{ENTER}") ; Start-Sleep 2
+        [Windows.Forms.SendKeys]::SendWait("{ENTER}") ; Start-Sleep 2
+        [Windows.Forms.SendKeys]::SendWait("{TAB}" * 2) ; Start-Sleep 2 ; [Windows.Forms.SendKeys]::SendWait("{ENTER}") ; Start-Sleep 2
+        [Windows.Forms.SendKeys]::SendWait("{ENTER}") ; Start-Sleep 2
+        [Windows.Forms.SendKeys]::SendWait("{TAB}" * 3) ; Start-Sleep 2 ; [Windows.Forms.SendKeys]::SendWait("{ENTER}") ; Start-Sleep 2
+        [Windows.Forms.SendKeys]::SendWait("{ENTER}") ; Start-Sleep 6
+        [Windows.Forms.SendKeys]::SendWait("%{F4}") ; Start-Sleep 2
+    }
+    
+}
+
+Function Update-Flutter {
+
+    $Deposit = "$Env:LocalAppData\Android\Flutter"
+    Update-Git ; Invoke-Expression "git clone https://github.com/flutter/flutter.git -b stable $Deposit"
+    Update-SysPath "$Deposit\bin" "Machine"
+
+    Invoke-Expression "flutter channel stable"
+    Invoke-Expression "flutter precache" ; Invoke-Expression "flutter upgrade"
+    Invoke-Expression "echo $("yes " * 10) | flutter doctor --android-licenses"
+    Invoke-Expression "dart --disable-analytics"
+    Invoke-Expression "flutter config --no-analytics"
+
+    $Product = "$Env:ProgramFiles\Android\Android Studio"
+    Update-JetbrainsPlugin "$Product" "6351"  # dart
+    Update-JetbrainsPlugin "$Product" "9212"  # flutter
+    Update-JetbrainsPlugin "$Product" "13666" # flutter-intl
+    Update-JetbrainsPlugin "$Product" "14641" # flutter-riverpod-snippets
+
+    Start-Process "code" "--install-extension Dart-Code.flutter --force" -WindowStyle Hidden -Wait
+    Start-Process "code" "--install-extension alexisvt.flutter-snippets --force" -WindowStyle Hidden -Wait
+    Start-Process "code" "--install-extension pflannery.vscode-versionlens --force" -WindowStyle Hidden -Wait
+    Start-Process "code" "--install-extension robert-brunhage.flutter-riverpod-snippets --force" -WindowStyle Hidden -Wait
+    Start-Process "code" "--install-extension usernamehw.errorlens --force" -WindowStyle Hidden -Wait
+
+}
+
+Function Update-Git {
+
+    Param (
+        [String] $Default = "main",
+        [String] $GitMail,
+        [String] $GitUser
+    )
+    
+    $Starter = "$Env:ProgramFiles\Git\git-bash.exe"
+    $Current = Expand-Version "$Starter"
+    $Address = "https://git-scm.com/download/win"
+    $Version = Invoke-Scraper "HtmlContent" "$Address" "<strong>([\d.]+)</strong>"
+    $Updated = [Version] "$Current" -Ge [Version] "$Version"
+    If (-Not $Updated) {
+        $Address = "https://github.com/git-for-windows/git/releases/download"
+        $Address = "$Address/v$Version.windows.1/Git-$Version-64-bit.exe"
+        $Fetched = Invoke-Fetcher "$Address"
+        $ArgList = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART, /NOCANCEL, /SP- /COMPONENTS=`"`""
+        Invoke-Gsudo { Start-Process "$Using:Fetched" "$Using:ArgList" -Wait }
+    }
+    
+    Update-SysPath "$Env:ProgramFiles\Git\cmd" "Process"
+    If ($Null -Ne $GitMail) { Invoke-Expression "git config --global user.email '$GitMail'" }
+    If ($Null -Ne $GitUser) { Invoke-Expression "git config --global user.name '$GitUser'" }
+    Invoke-Expression "git config --global http.postBuffer 1048576000"
+    Invoke-Expression "git config --global init.defaultBranch '$Default'"
+    
+}
+
 Function Update-Gsudo {
 
     $Starter = "${Env:ProgramFiles(x86)}\gsudo\gsudo.exe"
@@ -244,6 +366,38 @@ Function Update-Gsudo {
         Return $False
     }
     
+}
+
+Function Update-JetbrainsPlugin {
+
+    Param(
+        [String] $Deposit = "$Env:ProgramFiles\Android\Android Studio",
+        [String] $Element
+    )
+
+    If (-Not (Test-Path "$Deposit") -Or ([String]::IsNullOrWhiteSpace($Element))) { Return 0 }
+    $Release = (Get-Content "$Deposit\product-info.json" | ConvertFrom-Json).buildNumber
+    $Release = [Regex]::Matches("$Release", "([\d.]+)\.").Groups[1].Value
+    $DataDir = (Get-Content "$Deposit\product-info.json" | ConvertFrom-Json).dataDirectoryName
+    $Adjunct = If ("$DataDir" -Like "AndroidStudio*") { "Google\$DataDir" } Else { "JetBrains\$DataDir" }
+    $Plugins = "$Env:AppData\$Adjunct\plugins" ; New-Item "$Plugins" -ItemType Directory -EA SI
+    For ($I = 0; $I -Le 3; $I++) {
+        For ($J = 0; $J -Le 19; $J++) {
+            $Address = "https://plugins.jetbrains.com/api/plugins/$Element/updates?page=$I"
+            $Content = Invoke-Scraper "JsonContent" "$Address"
+            $Maximum = $Content["$J"].until.Replace("`"", "").Replace("*", "9999")
+            $Minimum = $Content["$J"].since.Replace("`"", "").Replace("*", "9999")
+            If ([Version] "$Minimum" -Le "$Release" -And "$Release" -Le "$Maximum") {
+                $Address = $Content["$J"].file.Replace("`"", "")
+                $Address = "https://plugins.jetbrains.com/files/$Address"
+                $Fetched = Invoke-Fetcher "$Address"
+                Expand-Archive "$Fetched" "$Plugins"
+                Break 2
+            }
+            Start-Sleep 1
+        }
+    }
+
 }
 
 Function Update-Nanazip {
@@ -295,6 +449,23 @@ Function Update-Nvidia {
 
 }
 
+Function Update-Temurin {
+
+    $Current = Expand-Version "*Temurin*"
+    $Present = "$Current" -Ne "0.0.0.0"
+    $Address = "https://api.github.com/repos/adoptium/temurin19-binaries/releases/latest"
+    $Version = Invoke-Scraper "GithubVersion" "$Address"
+    $Updated = [Version] "$Current" -Ge [Version] "$Version"
+    If (-Not $Updated) {
+        $Address = Invoke-Scraper "GithubRelease" "$Address" "*jdk_x64_windows*.msi"
+        $Fetched = Invoke-Fetcher "$Address"
+        $Adjunct = If ($Present) { "REINSTALL=ALL REINSTALLMODE=amus" } Else { "INSTALLLEVEL=1" }
+        Invoke-Gsudo { Start-Process "msiexec" "/i `"$Using:Fetched`" $Using:Adjunct /quiet" -Wait }
+        Update-SysPath -Deposit "" -Section "Machine"
+    }
+
+}
+
 Function Update-Wsa {
 
     $Current = Expand-Version "*WindowsSubsystemForAndroid*"
@@ -341,6 +512,9 @@ Function Main {
     $Factors = @(
         "Update-Nvidia Cuda"
         "Update-Wsa"
+        "Update-AndroidStudio"
+        "Update-Git -GitMail 72373746+sharpordie@users.noreply.github.com -GitUser sharpordie"
+        "Update-Flutter"
     )
     
     # Output progress
