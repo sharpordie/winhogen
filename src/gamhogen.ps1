f3839b14c4ca82c7410a93e3b354808019ef8dca
sharpordie <72373746+sharpordie@users.noreply.github.com>
2023-02-07 14:28:28 +0100

    small update

--- src/gamhogen.ps1
+++ src/gamhogen.ps1
@@ -1,11 +1,9 @@
 #!/usr/bin/env pwsh
 
 Clear-Host
-
 $Current = $Script:MyInvocation.MyCommand.Path
 $Host.UI.RawUI.WindowTitle = (Get-Item "$Current").BaseName
-echo "(Split-Path "$Current")\winhogen.psm1"
-Import-Module "(Split-Path "$Current")\winhogen.psm1" -Force
+Import-Module "$(Split-Path "$Current")\winhogen.psm1" -Force
 
 Write-Output "+-------------------------------------------------------------------------+"
 Write-Output "|                                                                         |"
