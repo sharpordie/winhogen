a625bc0493a85a7dbbfdc548047181dbb97fc771
sharpordie <72373746+sharpordie@users.noreply.github.com>
2023-02-07 10:05:12 +0000

    small update

---
+++ src/Invoke-Flaui.ps1
@@ -0,0 +1,36 @@
+#!/usr/bin/env pwsh
+
+Function Update-Ldplayer {
+
+    $Current = (Get-Package "LDPlayer" -ProviderName "Programs" -EA SI).Version
+    If ($Null -Eq $Current) { $Current = "0.0.0.0" }
+    $Present = $Current -Ne "0.0.0.0"
+
+    $Fetched = "LDPlayer_9.0.36.exe"
+   
+    Invoke-Gsudo {
+        Add-Type -Path "Libs\Interop.UIAutomationClient.dll"
+        Add-Type -Path "Libs\FlaUI.Core.dll"
+        Add-Type -Path "Libs\FlaUI.UIA3.dll"
+        Add-Type -Path "Libs\System.Drawing.Common.dll"
+        Add-Type -Path "Libs\System.Security.Permissions.dll"
+        $Handler = [FlaUI.UIA3.UIA3Automation]::New()
+        $Invoked = [FlaUI.Core.Application]::Launch("$Using:Fetched")
+        $Window1 = $Invoked.GetMainWindow($Handler)
+        $Window1.Focus()
+        $Scraped = $Window1.BoundingRectangle
+        $FactorX = $Scraped.X + ($Scraped.Width / 2)
+        $FactorY = $Scraped.Y + ($Scraped.Height / 2) + 60
+        $Centrum = [Drawing.Point]::New($FactorX, $FactorY)
+        Start-Sleep 4
+        [FlaUI.Core.Input.Mouse]::LeftClick($Centrum);
+        Start-Sleep 50
+        $Factor1 = [FlaUI.Core.WindowsAPI.VirtualKeyShort]::ALT
+        $Factor2 = [FlaUI.Core.WindowsAPI.VirtualKeyShort]::F4
+        [FlaUI.Core.Input.Keyboard]::TypeSimultaneously($Factor1, $Factor2)
+    } 
+    
+
+}
+
+Update-Ldplayer
