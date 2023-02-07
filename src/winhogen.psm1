3a0f68f9979e0ecd42d80087307d91f51bbdf41f
sharpordie <72373746+sharpordie@users.noreply.github.com>
2023-02-07 14:48:33 +0100

    small update

--- src/winhogen.psm1
+++ src/winhogen.psm1
@@ -15,33 +15,27 @@ Function Update-Ldplayer {
         $Address = "https://encdn.ldmnq.com/download/package/LDPlayer_$Version.exe"
         $Fetched = Join-Path "$Env:Temp" "$(Split-Path "$Address" -Leaf)"
         # (New-Object Net.WebClient).DownloadFile("$Address", "$Fetched")
-
-        $Current = Split-Path $script:MyInvocation.MyCommand.Path
-        echo "VALUE: $Current"
-
+        $Current = Split-Path $Script:MyInvocation.MyCommand.Path
         Invoke-Gsudo {
-            $Current = Split-Path $script:MyInvocation
-            echo "VALUE: $Current"
-            # $Current = Split-Path $Script:PSScriptRoot
-            # Add-Type -Path "$Current\libs\Interop.UIAutomationClient.dll"
-            # Add-Type -Path "$Current\libs\FlaUI.Core.dll"
-            # Add-Type -Path "$Current\libs\FlaUI.UIA3.dll"
-            # Add-Type -Path "$Current\libs\System.Drawing.Common.dll"
-            # Add-Type -Path "$Current\libs\System.Security.Permissions.dll"
-            # $Handler = [FlaUI.UIA3.UIA3Automation]::New()
-            # $Started = [FlaUI.Core.Application]::Launch("$Using:Fetched")
-            # $Window1 = $Started.GetMainWindow($Handler)
-            # $Window1.Focus()
-            # $Scraped = $Window1.BoundingRectangle
-            # $FactorX = $Scraped.X + ($Scraped.Width / 2)
-            # $FactorY = $Scraped.Y + ($Scraped.Height / 2) + 60
-            # $Centrum = [Drawing.Point]::New($FactorX, $FactorY)
-            # Start-Sleep 4
-            # [FlaUI.Core.Input.Mouse]::LeftClick($Centrum)
-            # Start-Sleep 50
-            # $Factor1 = [FlaUI.Core.WindowsAPI.VirtualKeyShort]::ALT
-            # $Factor2 = [FlaUI.Core.WindowsAPI.VirtualKeyShort]::F4
-            # [FlaUI.Core.Input.Keyboard]::TypeSimultaneously($Factor1, $Factor2)
+            Add-Type -Path "$Using:Current\libs\Interop.UIAutomationClient.dll"
+            Add-Type -Path "$Using:Current\libs\FlaUI.Core.dll"
+            Add-Type -Path "$Using:Current\libs\FlaUI.UIA3.dll"
+            Add-Type -Path "$Using:Current\libs\System.Drawing.Common.dll"
+            Add-Type -Path "$Using:Current\libs\System.Security.Permissions.dll"
+            $Handler = [FlaUI.UIA3.UIA3Automation]::New()
+            $Started = [FlaUI.Core.Application]::Launch("$Using:Fetched")
+            $Window1 = $Started.GetMainWindow($Handler)
+            $Window1.Focus()
+            $Scraped = $Window1.BoundingRectangle
+            $FactorX = $Scraped.X + ($Scraped.Width / 2)
+            $FactorY = $Scraped.Y + ($Scraped.Height / 2) + 60
+            $Centrum = [Drawing.Point]::New($FactorX, $FactorY)
+            Start-Sleep 4
+            [FlaUI.Core.Input.Mouse]::LeftClick($Centrum)
+            Start-Sleep 50
+            $Factor1 = [FlaUI.Core.WindowsAPI.VirtualKeyShort]::ALT
+            $Factor2 = [FlaUI.Core.WindowsAPI.VirtualKeyShort]::F4
+            [FlaUI.Core.Input.Keyboard]::TypeSimultaneously($Factor1, $Factor2)
         }
 
         Remove-Item "$Env:Public\Desktop\LDM*.lnk" -EA SI
