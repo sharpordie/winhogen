d3857237d3524c75c101011d84b2c9f6bce8633d
sharpordie <72373746+sharpordie@users.noreply.github.com>
2023-02-07 14:40:01 +0100

    small update

--- src/winhogen.psm1
+++ src/winhogen.psm1
@@ -17,26 +17,28 @@ Function Update-Ldplayer {
 		# (New-Object Net.WebClient).DownloadFile("$Address", "$Fetched")
 
         Invoke-Gsudo {
-            $Current = Split-Path $Script:PSScriptRoot
-            Add-Type -Path "$Current\libs\Interop.UIAutomationClient.dll"
-            Add-Type -Path "$Current\libs\FlaUI.Core.dll"
-            Add-Type -Path "$Current\libs\FlaUI.UIA3.dll"
-            Add-Type -Path "$Current\libs\System.Drawing.Common.dll"
-            Add-Type -Path "$Current\libs\System.Security.Permissions.dll"
-            $Handler = [FlaUI.UIA3.UIA3Automation]::New()
-            $Started = [FlaUI.Core.Application]::Launch("$Using:Fetched")
-            $Window1 = $Started.GetMainWindow($Handler)
-            $Window1.Focus()
-            $Scraped = $Window1.BoundingRectangle
-            $FactorX = $Scraped.X + ($Scraped.Width / 2)
-            $FactorY = $Scraped.Y + ($Scraped.Height / 2) + 60
-            $Centrum = [Drawing.Point]::New($FactorX, $FactorY)
-            Start-Sleep 4
-            [FlaUI.Core.Input.Mouse]::LeftClick($Centrum)
-            Start-Sleep 50
-            $Factor1 = [FlaUI.Core.WindowsAPI.VirtualKeyShort]::ALT
-            $Factor2 = [FlaUI.Core.WindowsAPI.VirtualKeyShort]::F4
-            [FlaUI.Core.Input.Keyboard]::TypeSimultaneously($Factor1, $Factor2)
+            $Current = $PSCommandPath | Split-Path -Parent
+            echo $Current
+            # $Current = Split-Path $Script:PSScriptRoot
+            # Add-Type -Path "$Current\libs\Interop.UIAutomationClient.dll"
+            # Add-Type -Path "$Current\libs\FlaUI.Core.dll"
+            # Add-Type -Path "$Current\libs\FlaUI.UIA3.dll"
+            # Add-Type -Path "$Current\libs\System.Drawing.Common.dll"
+            # Add-Type -Path "$Current\libs\System.Security.Permissions.dll"
+            # $Handler = [FlaUI.UIA3.UIA3Automation]::New()
+            # $Started = [FlaUI.Core.Application]::Launch("$Using:Fetched")
+            # $Window1 = $Started.GetMainWindow($Handler)
+            # $Window1.Focus()
+            # $Scraped = $Window1.BoundingRectangle
+            # $FactorX = $Scraped.X + ($Scraped.Width / 2)
+            # $FactorY = $Scraped.Y + ($Scraped.Height / 2) + 60
+            # $Centrum = [Drawing.Point]::New($FactorX, $FactorY)
+            # Start-Sleep 4
+            # [FlaUI.Core.Input.Mouse]::LeftClick($Centrum)
+            # Start-Sleep 50
+            # $Factor1 = [FlaUI.Core.WindowsAPI.VirtualKeyShort]::ALT
+            # $Factor2 = [FlaUI.Core.WindowsAPI.VirtualKeyShort]::F4
+            # [FlaUI.Core.Input.Keyboard]::TypeSimultaneously($Factor1, $Factor2)
         }
 
         Remove-Item "$Env:Public\Desktop\LDM*.lnk" -EA SI
