238ad3e235b1c4deca3531e04ca25c4dac2afcda
sharpordie <72373746+sharpordie@users.noreply.github.com>
2023-02-11 10:33:49 +0100

    small update

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ Opinionated post-installation scripts for Windows 11.
 
 # <samp>GUIDANCE</samp>
 
+```powershell
+iwr https://github.com/sharpordie/winhogen/archive/refs/heads/main.zip -o $env:temp\main.zip
+expand-archive $env:temp\main.zip -destinationpath $env:temp -force
+powershell -ep bypass $env:temp\winhogen-main\src\winhogen.ps1
+```
+
+<!-- 
 ## For coding purpose
 
 <img src="https://fakeimg.pl/852x480/43d6b5/43d6b5" width="49.25%"/><img src="assets/img0.png" width="1.5%"/><img src="https://fakeimg.pl/852x480/43d6b5/43d6b5" width="49.25%"/>
@@ -66,4 +73,4 @@ Copy the generic template and make it yours.
 ```powershell
 git clone https://github.com/sharpordie/winhogen.git
 cp winhogen\src\template.ps1 winhogen\src\cushogen.ps1
-```
+``` -->
--- src/winhogen.ps1
+++ src/winhogen.ps1
@@ -397,3 +397,74 @@ Function Update-Windows {
     Enable-Feature "RemoteDesktop"
 
 }
+
+Function Main {
+
+    # Change headline
+    Clear-Host ; $Current = $Script:MyInvocation.MyCommand.Path
+    $Host.UI.RawUI.WindowTitle = (Get-Item "$Current").BaseName.ToUpper()
+
+    # Output greeting
+    Write-Output "+---------------------------------------------------------------+"
+    Write-Output "|                                                               |"
+    Write-Output "|  > CODHOGEN                                                   |"
+    Write-Output "|                                                               |"
+    Write-Output "|  > CONFIGURATION SCRIPT FOR CODING PURPOSE                    |"
+    Write-Output "|                                                               |"
+    Write-Output "+---------------------------------------------------------------+"
+
+    # Import winhogen
+    Import-Module "$(Split-Path "$Current")\winhogen.psm1" -Force
+
+    # Handle security
+    $Loading = "`nTHE UPDATING DEPENDENCIES PROCESS HAS LAUNCHED"
+    $Failure = "`rTHE UPDATING DEPENDENCIES PROCESS WAS CANCELED"
+    Write-Host "$Loading" -FO DarkYellow -NoNewline
+    Remove-Feature "Uac" ; Update-Element "Plan" "Ultimate"
+    $Correct = (Update-Gsudo) -And ! (gsudo cache on -d -1 2>&1).ToString().Contains("Error")
+    If (-Not $Correct) { Write-Host "$Failure`n" -FO Red ; Exit } ; Update-Powershell
+
+    Invoke-Browser -Firefox -Visible ; Exit
+
+    # Handle elements
+    $Members = @(
+        "Invoke-Browser -Firefox -Visible"
+        # "Update-Windows 'Romance Standard Time' 'CODHOGEN'"
+        # "Update-Ldplayer"
+    )
+
+    # Output progress
+    $Maximum = (65 - 20) * -1
+    $Shaping = "`r{0,$Maximum}{1,-3}{2,-6}{3,-3}{4,-8}"
+    $Heading = "$Shaping" -F "FUNCTION", " ", "STATUS", " ", "DURATION"
+    Write-Host "$Heading"
+    Foreach ($Element In $Members) {
+        $Started = Get-Date
+        $Running = $Element.Split(' ')[0].ToUpper()
+        $Shaping = "`n{0,$Maximum}{1,-3}{2,-6}{3,-3}{4,-8}"
+        $Loading = "$Shaping" -F "$Running", "", "ACTIVE", "", "--:--:--"
+        Write-Host "$Loading" -ForegroundColor DarkYellow -NoNewline
+        Try {
+            Invoke-Expression $Element *> $Null
+            $Elapsed = "{0:hh}:{0:mm}:{0:ss}" -F ($(Get-Date) - $Started)
+            $Shaping = "`r{0,$Maximum}{1,-3}{2,-6}{3,-3}{4,-8}"
+            $Success = "$Shaping" -F "$Running", "", "WORKED", "", "$Elapsed"
+            Write-Host "$Success" -ForegroundColor Green -NoNewLine
+        }
+        Catch {
+            $Elapsed = "{0:hh}:{0:mm}:{0:ss}" -F ($(Get-Date) - $Started)
+            $Shaping = "`r{0,$Maximum}{1,-3}{2,-6}{3,-3}{4,-8}"
+            $Failure = "$Shaping" -F "$Running", "", "FAILED", "", "$Elapsed"
+            Write-Host "$Failure" -ForegroundColor Red -NoNewLine
+        }
+    }
+
+    # Revert security
+    Enable-Feature "Uac" ; gsudo -k *> $Null
+
+    # Output new line
+    Write-Host "`n"
+
+}
+
+Main
