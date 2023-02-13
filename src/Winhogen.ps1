4275316e8a57b868e229d97c7b489441bdc6f883
sharpordie <72373746+sharpordie@users.noreply.github.com>
2023-02-13 13:42:44 +0100

    small update

--- src/Winhogen.ps1
+++ src/Winhogen.ps1
@@ -163,10 +163,9 @@ Function Invoke-Scraper {
         # $Scraped = $WebPage.ContentAsync().GetAwaiter().GetResult()
         $Scraped = $WebPage.QuerySelectorAsync("body > pre").GetAwaiter().GetResult()
         $Scraped = $Scraped.InnerTextAsync().GetAwaiter().GetResult().ToString()
-        echo $Scraped
         $WebPage.CloseAsync().GetAwaiter().GetResult()
         $Browser.CloseAsync().GetAwaiter().GetResult()
-        $Scraped
+        Return $Scraped
     }
 
     # Try {
