75382677e50f704d0265878b6a2f72a6ffb7ab4a
sharpordie <72373746+sharpordie@users.noreply.github.com>
2023-02-13 14:23:02 +0100

    small update

--- src/Winhogen.ps1
+++ src/Winhogen.ps1
@@ -371,6 +371,8 @@ Function Update-Gsudo {
     $Present = $Current -Ne "0.0.0.0"
 
     $Address = "https://api.github.com/repos/gerardog/gsudo/releases/latest"
+    $dd = Invoke-Scraper "$Address"
+    $dd
     $Version = [Regex]::Match((Invoke-Scraper "$Address" | ConvertFrom-Json).tag_name, "[\d.]+").Value
     # $Address = "https://github.com/gerardog/gsudo/releases/latest"
     # $Version = [Regex]::Matches((Invoke-Scraper "$Address"), "gsudo v([\d.]+)").Groups[1].Value
