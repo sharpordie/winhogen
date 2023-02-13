6256a7b7b18050e05a2a5ce7f033cb4a52f5322b
sharpordie <72373746+sharpordie@users.noreply.github.com>
2023-02-13 15:11:58 +0100

    small update

--- src/Winhogen.ps1
+++ src/Winhogen.ps1
@@ -362,6 +362,7 @@ Function Update-Bluestacks {
     # $Present = $Current -Ne "0.0.0.0"
 
     $Address = "https://support.bluestacks.com/hc/en-us/articles/4402611273485-BlueStacks-5-offline-installer"
+    Invoke-Scraper "Html" "$Address" ; exit
     $Results = [Regex]::Matches((Invoke-Scraper "Html" "$Address"), "windows/nxt/([\d.]+)/(?<sha>[0-9a-f]+)/")
     $Version = $Results.Groups[1].Value
     $Hashing = $results.Groups[2].Value
