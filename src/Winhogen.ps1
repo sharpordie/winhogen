ca9ac81f8b8c0c5cc6dcfff52a2c2e4f2085d8c1
sharpordie <72373746+sharpordie@users.noreply.github.com>
2023-02-16 06:46:05 +0100

    small update

--- src/Winhogen.ps1
+++ src/Winhogen.ps1
@@ -433,12 +433,14 @@ Function Update-Antidote {
 
         $Window1 = $Started.GetMainWindow($Handler)
         $Window1.Focus()
-        $Button1 = $Window1.FindFirstDescendant($Handler.ConditionFactory.ByControlType([FlaUI.Core.Definitions.ControlType]::Button))
-        $Button1.Click() ; Start-Sleep 4
+        $Buttons = $Window1.FindAllChildren($Handler.ConditionFactory.ByControlType([FlaUI.Core.Definitions.ControlType]::Button))
+        $Buttons
+        # $Button1 = $Window1.FindFirstDescendant($Handler.ConditionFactory.ByControlType([FlaUI.Core.Definitions.ControlType]::Button))
+        # $Button1.Click() ; Start-Sleep 4
 
-        $Window2 = $Started.GetMainWindow($Handler)
-        $Button2 = $Window2.FindFirstDescendant($Handler.ConditionFactory.ByName("Enter a serial number…")).AsButton()
-        $Button2.Click() ; Start-Sleep 4
+        # $Window2 = $Started.GetMainWindow($Handler)
+        # $Button2 = $Window2.FindFirstDescendant($Handler.ConditionFactory.ByName("Enter a serial number…")).AsButton()
+        # $Button2.Click() ; Start-Sleep 4
 
         # Start Antidote
         # Press ENTER
