5c7a1d89e514d1ddb9955f875b927242abb8ed18
sharpordie <72373746+sharpordie@users.noreply.github.com>
2023-02-19 18:47:40 +0100

    small update

--- src/Winhogen.ps1
+++ src/Winhogen.ps1
@@ -148,15 +148,16 @@ Function Invoke-Browser {
     Import-Library "Microsoft.Bcl.AsyncInterfaces"
     Import-Library "Microsoft.CodeAnalysis"
     Import-Library "Microsoft.Playwright"
-    $Current = $Script:MyInvocation.MyCommand.Path
-    $Null = Invoke-Gsudo {
-        . $Using:Current ; Start-Sleep 4
-        Import-Library "System.Text.Json"
-        Import-Library "Microsoft.Bcl.AsyncInterfaces"
-        Import-Library "Microsoft.CodeAnalysis"
-        Import-Library "Microsoft.Playwright"
-        [Microsoft.Playwright.Program]::Main(@("install", "chromium"))
-    }
+    # $Current = $Script:MyInvocation.MyCommand.Path
+    # $Null = Invoke-Gsudo {
+    #     . $Using:Current
+    #     Import-Library "System.Text.Json"
+    #     Import-Library "Microsoft.Bcl.AsyncInterfaces"
+    #     Import-Library "Microsoft.CodeAnalysis"
+    #     Import-Library "Microsoft.Playwright"
+    #     [Microsoft.Playwright.Program]::Main(@("install", "chromium"))
+    # }
+    [Microsoft.Playwright.Program]::Main(@("install", "chromium"))
     [Microsoft.Playwright.Playwright]::CreateAsync().GetAwaiter().GetResult()
 }
 
@@ -187,16 +188,17 @@ Function Invoke-Extract {
 
     Param (
         [String] $Archive,
+        [String] $Deposit,
         [String] $Secrets
     )
 
-    # If (-Not (Test-Path "$Env:LocalAppData\Microsoft\WindowsApps\7z.exe")) { Update-Nanazip }
-    # If (-Not $Deposit) { $Deposit = [IO.Directory]::CreateDirectory("$Env:Temp\$([Guid]::NewGuid().Guid)").FullName }
-    # If (-Not (Test-Path "$Deposit")) { New-Item "$Deposit" -ItemType Directory -EA SI }
-    $Deposit = [IO.Directory]::CreateDirectory("$Env:Temp\$([Guid]::NewGuid().Guid)").FullName
-    Start-Process "7z.exe" "x `"$Archive`" -o`"$Deposit`" -p`"$Secrets`" -y -bso0 -bsp0" -WindowStyle Hidden -Wait
-    # & "$Env:LocalAppData\Microsoft\WindowsApps\7z.exe" x "$Archive" -o"$Deposit" -p"$Secrets" -y -bso0 -bsp0
-    $Deposit
+    If (-Not (Test-Path "$Env:LocalAppData\Microsoft\WindowsApps\7z.exe")) { Update-Nanazip }
+    If (-Not $Deposit) { $Deposit = [IO.Directory]::CreateDirectory("$Env:Temp\$([Guid]::NewGuid().Guid)").FullName }
+    If (-Not (Test-Path "$Deposit")) { New-Item "$Deposit" -ItemType Directory -EA SI }
+    # $Deposit = [IO.Directory]::CreateDirectory("$Env:Temp\$([Guid]::NewGuid().Guid)").FullName
+    # Start-Process "7z.exe" "x `"$Archive`" -o`"$Deposit`" -p`"$Secrets`" -y -bso0 -bsp0" -WindowStyle Hidden -Wait
+    & "$Env:LocalAppData\Microsoft\WindowsApps\7z.exe" x "$Archive" -o"$Deposit" -p"$Secrets" -y -bso0 -bsp0
+    "$Deposit"
 
 }
 
@@ -451,36 +453,32 @@ Function Update-Antidote {
     $Current = Try { (Get-Command "$Starter" -EA SI).Version.ToString() } Catch { "0.0.0.0" }
     $Present = $Current -Ne "0.0.0.0"
 
-    # $Address = "https://filecr.com/windows/antidote"
-    # $Results = [Regex]::Matches((Invoke-Scraper "Html" "$Address"), "<title>Antidote ([\d]+) v([\d.]+) .*</title>")
-    # $Version = "$($Results.Groups[1].Value).$($Results.Groups[2].Value)"
-    # $Updated = [Version] "$Current" -Ge [Version] "$Version"
-
-    $Address = "https://filecr.com/windows/lamnisoft-fontexplorerl-m"
-    $Results = [Regex]::Matches((Invoke-Scraper "Html" "$Address"), "<title>Lanmisoft FontExplorerL.M ([\d.]+) .*</title>")
-    $Version = $Results.Groups[1].Value
+    $Address = "https://filecr.com/windows/antidote"
+    $Results = [Regex]::Matches((Invoke-Scraper "Html" "$Address"), "<title>Antidote ([\d]+) v([\d.]+) .*</title>")
+    $Version = "$($Results.Groups[1].Value).$($Results.Groups[2].Value)"
     $Updated = [Version] "$Current" -Ge [Version] "$Version"
+
+    # $Address = "https://filecr.com/windows/lamnisoft-fontexplorerl-m"
+    # $Results = [Regex]::Matches((Invoke-Scraper "Html" "$Address"), "<title>Lanmisoft FontExplorerL.M ([\d.]+) .*</title>")
+    # $Version = $Results.Groups[1].Value
+    # $Updated = [Version] "$Current" -Ge [Version] "$Version"
 
     If (-Not $Updated) {
         $Fetched = "$(Invoke-Fetcher "Filecr" "$Address")".Trim()
         # $Fetched = "$Fetched".Trim()
-        Write-Output "5555BLALALALALALALAL`n"
-        Write-Output "'$Fetched'"
-        Write-Output "'$("$Fetched".Trim())'"
         # $Fetched = "C:\Users\Admin\AppData\Local\Temp\Antidote 11 v3.2 [FileCR].zip"
-        # $Deposit = Invoke-Extract -Archive "$Fetched" -Secrets "123"
+        $Deposit = "$(Invoke-Extract -Archive "$Fetched" -Secrets "123")".Trim()
         # Update-Nanazip
-        $Extract = [IO.Directory]::CreateDirectory("$Env:Temp\$([Guid]::NewGuid().Guid)").FullName
-        & "$Env:LocalAppData\Microsoft\WindowsApps\7z.exe" x "$("$Fetched".Trim())" -o"$Extract" -p"123" -y
+        # $Extract = [IO.Directory]::CreateDirectory("$Env:Temp\$([Guid]::NewGuid().Guid)").FullName
+        # & "$Env:LocalAppData\Microsoft\WindowsApps\7z.exe" x "$("$Fetched".Trim())" -o"$Extract" -p"123" -y
         # & "$Env:LocalAppData\Microsoft\WindowsApps\7z.exe" x "$Fetched" -o"$Extract" -p"123" -y
-        Exit
         # Start-Process "7z.exe" "x `"$Fetched`" -o`"$Extract`" -p`"123`" -y -bso0 -bsp0" -WindowStyle Hidden -Wait
         # $Deposit = "C:\Users\Admin\AppData\Local\Temp\ad5d8b05-1c49-4a88-af84-9b1eb48bcf9b"
-        $RootDir = (Get-Item "$Extract\Ant*\Ant*").FullName
+        $RootDir = (Get-Item "$Deposit\Ant*\Ant*").FullName
         $Archive = (Get-Item "$RootDir\Anti*.exe").FullName
-        # $Extract = Invoke-Extract -Archive "$Archive"
-        $Extract = [IO.Directory]::CreateDirectory("$Env:Temp\$([Guid]::NewGuid().Guid)").FullName
-        & "$Env:LocalAppData\Microsoft\WindowsApps\7z.exe" x "$Archive" -o"$Extract" -y -bso0 -bsp0
+        $Extract = "$(Invoke-Extract -Archive "$Archive")".Trim()
+        # $Extract = [IO.Directory]::CreateDirectory("$Env:Temp\$([Guid]::NewGuid().Guid)").FullName
+        # & "$Env:LocalAppData\Microsoft\WindowsApps\7z.exe" x "$Archive" -o"$Extract" -y -bso0 -bsp0
         # Start-Process "7z.exe" "x `"$Archive`" -o`"$Extract`" -y -bso0 -bsp0" -WindowStyle Hidden -Wait
         # $Extract = "C:\Users\Admin\AppData\Local\Temp\dd4e2e4a-dea1-48c2-b4e8-b67f2159e8c0"
         $Modules = (Get-Item "$Extract\*\msi\druide").FullName
