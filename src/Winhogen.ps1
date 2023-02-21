554c187bdda6a17e03792df0b75847ce7686ffab
sharpordie <72373746+sharpordie@users.noreply.github.com>
2023-02-21 11:52:42 +0100

    small update

--- src/Winhogen.ps1
+++ src/Winhogen.ps1
@@ -640,8 +640,8 @@ Function Update-Jetbra {
         $Extract = Invoke-Extract "$Fetched" "$(Split-Path "$Deposit")"
         Rename-Item -Path "$Extract\jetbra" -NewName "$Deposit"
         $Scripts = "$Deposit\scripts"
-        Start-Process "cscript" "$Scripts\uninstall-current-user.vbs" -WorkingDirectory "$Scripts"
-        Start-Process "cscript" "$Scripts\install-current-user.vbs" -WorkingDirectory "$Scripts"
+        Start-Process "cscript" "$Scripts\uninstall-current-user.vbs" -WorkingDirectory "$Scripts" -NoNewWindow
+        Start-Process "cscript" "$Scripts\install-current-user.vbs" -WorkingDirectory "$Scripts" -NoNewWindow
     }
 
 }
