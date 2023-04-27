38ea0fdc48e9d4f8545af7daf932e8712626b162
sharpordie <72373746+sharpordie@users.noreply.github.com>
2023-04-27 13:18:57 +0200

    feat: small update

--- src/winhogen.ps1
+++ src/winhogen.ps1
@@ -228,7 +228,7 @@ Function Export-Members {
                 "Update-Pycharm"
                 "Update-Qbittorrent"
                 "Update-Scrcpy"
-                "Update-Ventura"
+                # "Update-Ventura"
                 "Update-YtDlg"
             )
         }
